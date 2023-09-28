d80cf348376bd3f3c1fc8081c6fc51a4471a7efe
Andre <a.shumilkin@outlook.com>
2023-09-28 08:27:39 +0000

    Create buffer.java

---
+++ buffer.java
@@ -0,0 +1,14 @@
+public class Level1 {
+    public static int squirrel(int N)
+    {
+        int factorial = 1;
+        while (N > 1){
+            factorial *= N;
+            N -= 1;
+        }
+        while(factorial > 9){
+            factorial /= 10;
+        }
+        return factorial;
+    }
+}
