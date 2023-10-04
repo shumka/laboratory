313f5f3dadf374f115cc48189ceb4b9fe9f0a98e
Andre <a.shumilkin@outlook.com>
2023-10-04 08:12:06 +0000

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -1,146 +1,33 @@
-import static java.lang.Math.sqrt;
+import java.util.ArrayList;
 
 public class Level1 {
-    public static String PatternUnlock(int N, int[] hits) {
-        String result = "";
-        double distance = 0;
-        for (int i = 0; i < N - 1; i++) {
-
-            if (hits[i] == 1) {
-                switch (hits[i + 1]) {
-                    case 2:
-                    case 6:
-                    case 9:
-                        distance += 1;
-                        break;
-                    case 5:
-                    case 8:
-                        distance += sqrt(2);
-                        break;
-                    default:
-                        break;
+    public static int [] WordSearch(int len, String s, String subs){
+        s = s.toLowerCase();
+        subs = subs.toLowerCase();
+        String[] subStrDirty;
+        ArrayList<String> subStrClean = new ArrayList<String>();
+        subStrDirty = s.split(" ");
+        for(int i = 0; i < subStrDirty.length-1; i++){
+            if( (subStrDirty[i].length() + subStrDirty[i+1].length()) <= len ){
+                subStrClean.add(subStrDirty[i] + " " + subStrDirty[i+1]);
+                if(i == subStrDirty.length - 3){
+                    subStrClean.add(subStrDirty[i+2]);
+                } else {
+                    i++;
                 }
-            } else if (hits[i] == 2) {
-                switch (hits[i + 1]) {
-                    case 1:
-                    case 3:
-                    case 5:
-                    case 8:
-                        distance += 1;
-                        break;
-                    case 4:
-                    case 6:
-                    case 7:
-                    case 9:
-                        distance += sqrt(2);
-                        break;
-                    default:
-                        break;
-                }
-            } else if (hits[i] == 3) {
-                switch (hits[i + 1]) {
-                    case 2:
-                    case 4:
-                    case 7:
-                        distance += 1;
-                        break;
-                    case 5:
-                    case 8:
-                        distance += sqrt(2);
-                        break;
-                    default:
-                        break;
-                }
-
-            } else if (hits[i] == 4) {
-                switch (hits[i + 1]) {
-                    case 3:
-                    case 5:
-                        distance += 1;
-                        break;
-                    case 2:
-                        distance += sqrt(2);
-                        break;
-                    default:
-                        break;
-                }
-
-            } else if (hits[i] == 5) {
-                switch (hits[i + 1]) {
-                    case 2:
-                    case 4:
-                    case 6:
-                        distance += 1;
-                        break;
-                    case 1:
-                    case 3:
-                        distance += sqrt(2);
-                        break;
-                    default:
-                        break;
-                }
-
-            } else if (hits[i] == 6) {
-                switch (hits[i + 1]) {
-                    case 1:
-                    case 5:
-                        distance += 1;
-                        break;
-                    case 2:
-                        distance += sqrt(2);
-                        break;
-                    default:
-                        break;
-                }
-
-            } else if (hits[i] == 7) {
-                switch (hits[i + 1]) {
-                    case 3:
-                    case 8:
-                        distance += 1;
-                        break;
-                    case 2:
-                        distance += sqrt(2);
-                        break;
-                    default:
-                        break;
-                }
-
-            } else if (hits[i] == 8) {
-                switch (hits[i + 1]) {
-                    case 2:
-                    case 7:
-                    case 9:
-                        distance += 1;
-                        break;
-                    case 1:
-                    case 3:
-                        distance += sqrt(2);
-                        break;
-                    default:
-                        break;
-                }
-
-            } else if (hits[i] == 9) {
-                switch (hits[i + 1]) {
-                    case 1:
-                    case 8:
-                        distance += 1;
-                        break;
-                    case 2:
-                        distance += sqrt(2);
-                        break;
-                    default:
-                        break;
-                }
-
+            } else {
+                subStrClean.add(subStrDirty[i]);
             }
-
         }
-        result = String.format("%.5f", distance);
-        result = result.replaceAll("0", "");
-        result = result.replaceAll(",", "");
-
+        int[] result = new int[subStrClean.size()-1];
+        for(int i = 0; i < subStrClean.size()-1; i++){
+            if(subStrClean.get(i).matches(".*\\b" + subs + "\\b.*")){
+                result[i] = 1;
+            } else {
+                result[i] = 0;
+            }
+        }
         return result;
     }
+
 }
