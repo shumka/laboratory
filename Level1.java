4590ad0b0fd584a1df8267b6f88a454987438714
Andre <a.shumilkin@outlook.com>
2023-10-04 12:57:29 +0000

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -1,58 +1,21 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Level1 {
-    public static int[] WordSearch(int len, String s, String subs) {
-        String[] lines = splitString(s, len);
+    public static int SumOfThe(int N, int[] data) {
+        int totalSum = 0;
 
-        int[] result = new int[lines.length];
-        for (int i = 0; i < lines.length; i++) {
-            result[i] = containsWord(lines[i], subs) ? 1 : 0;
+        for (int i = 0; i < N; i++) {
+            totalSum += data[i];
         }
 
-        return result;
-    }
-
-
-    private static String[] splitString(String s, int len) {
-        List<String> lines = new ArrayList<>();
-        String[] words = s.split(" ");
-        StringBuilder line = new StringBuilder();
-
-        if (words.length == 1) {
-            int length = s.length();
-            int numSubstrings = (int) Math.ceil((double) length / len);
-            String[] substrings = new String[numSubstrings];
-
-            int start = 0;
-            for (int i = 0; i < numSubstrings; i++) {
-                int end = Math.min(start + len, length);
-                substrings[i] = s.substring(start, end);
-                start += len;
+        int currentSum = 0;
+        for (int i = 0; i < N; i++) {
+            currentSum += data[i];
+            if (currentSum == totalSum) {
+                return data[i];
             }
-            System.out.println(Arrays.toString(substrings));
-            return substrings;
+            totalSum -= data[i];
         }
 
-        for (String word : words) {
-            if (line.length() + word.length() <= len) {
-                line.append(word).append(" ");
-            } else {
-                lines.add(line.toString().trim());
-                line = new StringBuilder(word + " ");
-            }
-        }
-
-        if (line.length() > 0) {
-            lines.add(line.toString().trim());
-        }
-
-        return lines.toArray(new String[0]);
-
+        return -1;
     }
 
-    private static boolean containsWord(String line, String word) {
-        return line.matches(".*\\b" + word + "\\b.*");
-    }
 }
