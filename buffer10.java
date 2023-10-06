68934f5944f29fb6f06827725df35d4adc023287
Andre <a.shumilkin@outlook.com>
2023-10-06 10:27:12 +0000

    Update buffer10.java

--- buffer10.java
+++ buffer10.java
@@ -1,20 +1,22 @@
-public static int PrintingCosts2(String Line) {
-        int totalCost = 0;
-        String costs = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_abcdefghijklmnopqrstuvwxyz{|}~";
-        int[] charCosts = {0, 9, 6, 24, 29, 22, 24, 3, 12, 12, 17, 13, 7, 7, 4, 10, 22, 19, 22, 23, 21, 27, 26, 16, 23, 26,
-        8, 11, 10, 14, 10, 15, 32, 24, 29, 20, 26, 26, 20, 25, 25, 18, 18, 21, 16, 28, 25, 26, 23, 31, 28,
-        25, 16, 23, 19, 26, 18, 14, 22, 18, 10, 18, 7, 8, 3, 23, 25, 17, 25, 23, 18, 30, 21, 15, 20, 21,
-        16, 22, 18, 20, 25, 25, 13, 21, 17, 17, 13, 19, 13, 24, 19, 18, 12, 18, 18, 9};
-
-        for (int i = 0; i < Line.length(); i++) {
-        char c = Line.charAt(i);
-        int index = costs.indexOf(c);
-        if (index != -1) {
-        totalCost += charCosts[index];
-        } else {
-        totalCost += 23;
-        }
-        }
-
-        return totalCost;
-        }
+public class Level1 {
+        public static int PrintingCosts(String Line) {
+                int totalCost = 0;
+                String costs = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_abcdefghijklmnopqrstuvwxyz{|}~";
+                int[] charCosts = {0, 9, 6, 24, 29, 22, 24, 3, 12, 12, 17, 13, 7, 7, 4, 10, 22, 19, 22, 23, 21, 27, 26, 16, 23, 26,
+                8, 11, 10, 14, 10, 15, 32, 24, 29, 20, 26, 26, 20, 25, 25, 18, 18, 21, 16, 28, 25, 26, 23, 31, 28,
+                25, 16, 23, 19, 26, 18, 14, 22, 18, 10, 18, 7, 8, 3, 23, 25, 17, 25, 23, 18, 30, 21, 15, 20, 21,
+                16, 22, 18, 20, 25, 25, 13, 21, 17, 17, 13, 19, 13, 24, 19, 18, 12, 18, 18, 9};
+        
+                for (int i = 0; i < Line.length(); i++) {
+                char c = Line.charAt(i);
+                int index = costs.indexOf(c);
+                if (index != -1) {
+                totalCost += charCosts[index];
+                } else {
+                totalCost += 23;
+                }
+                }
+        
+                return totalCost;
+         }
+}
