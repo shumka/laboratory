bc4325ba595ba9794ede12fc739e6da90082928c
Andre <a.shumilkin@outlook.com>
2023-10-09 12:28:19 +0000

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -1,29 +1,45 @@
 public class Level1 {
-   public static String MassVote(int N, int[] Votes) {
-        int totalVotes = 0;
-        int maxVotes = 0;
-        int winnerIndex = -1;
-        boolean isMajority = false;
-
-        for (int i = 0; i < N; i++) {
-            totalVotes += Votes[i];
-            if (Votes[i] > maxVotes) {
-                maxVotes = Votes[i];
-                winnerIndex = i;
-                isMajority = true;
-            } else if (Votes[i] == maxVotes) {
-                isMajority = false;
+    public static int[] UFO(int N, int[] data, Boolean octal) {
+        int[] result = new int[N];
+        if (octal) {
+            for (int i = 0; i < N; i++) {
+                result[i] = OctalToDecimal(data[i]);
+            }
+        } else {
+            for (int i = 0; i < N; i++) {
+                result[i] = HexToDecimal(data[i]);
             }
         }
+        return result;
+    }
 
-        double percentage = ((double) maxVotes / totalVotes) * 100;
-
-        if (isMajority && percentage > 50) {
-            return "majority winner " + (winnerIndex + 1);
-        } else if (isMajority && percentage <= 50) {
-            return "minority winner " + (winnerIndex + 1);
-        } else {
-            return "no winner";
+    public static int OctalToDecimal(int octal) {
+        int decimal = 0;
+        int n = 0;
+        while (true) {
+            if (octal == 0) {
+                break;
+            } else {
+                int temp = octal % 10;
+                decimal += temp * Math.pow(8, n);
+                octal = octal / 10;
+                n++;
+            }
         }
+        return decimal;
+    }
+
+    public static int HexToDecimal(int hex) {
+        int decimal = 0;
+        int power = 0;
+
+        while (hex > 0) {
+            int digit = hex % 10;
+            decimal += digit * Math.pow(16, power);
+            hex /= 10;
+            power++;
+        }
+
+        return decimal;
     }
 }
