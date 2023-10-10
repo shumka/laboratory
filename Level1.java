da1a1b9d457b3d667b0d4d9d0e5b19f940d4ae91
Andre <a.shumilkin@outlook.com>
2023-10-10 13:54:47 +0000

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -1,41 +1,14 @@
+import java.util.Arrays;
+
 public class Level1 {
-    public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2) {
-        char[][] map1 = convertToMatrix(H1, W1, S1);
-        char[][] map2 = convertToMatrix(H2, W2, S2);
+    public static int MaximumDiscount(int N, int[] price) {
+        Arrays.sort(price);
+        int discount = 0;
 
-        for (int i = 0; i <= H1 - H2; i++) {
-            for (int j = 0; j <= W1 - W2; j++) {
-                boolean match = true;
-
-                for (int k = 0; k < H2; k++) {
-                    for (int l = 0; l < W2; l++) {
-                        if (map1[i + k][j + l] != map2[k][l]) {
-                            match = false;
-                            break;
-                        }
-                    }
-                    if (!match) {
-                        break;
-                    }
-                }
-
-                if (match) {
-                    return true;
-                }
-            }
+        for (int i = N - 3; i >= 0; i -= 3) {
+            discount += price[i];
         }
 
-        return false;
-    }
-
-    private static char[][] convertToMatrix(int rows, int cols, String str) {
-        char[][] matrix = new char[rows][cols];
-        String[] rowsArray = str.split(" ");
-
-        for (int i = 0; i < rows; i++) {
-            matrix[i] = rowsArray[i].toCharArray();
-        }
-
-        return matrix;
+        return discount;
     }
 }
