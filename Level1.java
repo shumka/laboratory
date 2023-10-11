6ebe3b82ecbedb2e87e605f5dad9cafebbfe582d
Andre <a.shumilkin@outlook.com>
2023-10-11 11:30:53 +0000

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -1,35 +1,43 @@
 import java.util.*;
 
 public class Level1 {
-    public static boolean LineAnalysis(String line) {
+public static boolean LineAnalysis(String line) {
         int n = line.length();
 
         if (n == 1 && line.charAt(0) == '*') {
-            return true;
+        return true;
         }
 
         if (line.charAt(0) == '*' && line.charAt(1) == '*') {
-            return true;
+        return true;
         }
 
         if (!line.startsWith("*") || !line.endsWith("*")) {
-            return false;
+        return false;
         }
 
-        List<String> list = new ArrayList<>(Arrays.asList(line.split("\\*")));
-        list.removeIf(String::isEmpty);
-        String[] patterns = list.toArray(new String[0]);
+        List<String> listDot = new ArrayList<>(Arrays.asList(line.split("\\*")));
+        List<String> listStars = new ArrayList<>(Arrays.asList(line.split("\\.")));
+        listDot.removeIf(String::isEmpty);
+        listStars.removeIf(String::isEmpty);
+        String[] patternsDot = listDot.toArray(new String[0]);
+        String[] patternsStars = listStars.toArray(new String[0]);
 
-        int patternLength = patterns[0].length();
+        int patternDotLength = patternsDot[0].length();
+        int patternStarsLength = patternsStars[0].length();
 
-        System.out.println(Arrays.toString(patterns));
-        for (int i = 0; i < patterns.length; i++) {
-            if (patterns[i].length() != patternLength) {
-                return false;
-            }
+        for (String patternsStar : patternsStars) {
+        if (patternsStar.length() != patternStarsLength) {
+        return false;
+        }
+        }
+        for (String s : patternsDot) {
+        if (s.length() != patternDotLength) {
+        return false;
+        }
         }
 
         return true;
 
-    }
+        }
 }
