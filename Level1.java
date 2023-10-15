d6fa99c80b8b99cc5c451b66b4654d175d241033
Andre <a.shumilkin@outlook.com>
2023-10-15 11:04:52 +0000

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -1,29 +1,106 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
 
 public class Level1 {
-    public static String [] ShopOLAP(int N, String [] items) {
-        HashMap<String, Integer> salesArray = new HashMap<>(N);
+    private StringBuilder text;
+    private Deque<String> undoStack;
+    private Stack<String> redoStack;
+    public Level1() {
+        text = new StringBuilder();
+        undoStack = new ArrayDeque<>();
+        redoStack = new Stack<>();
+    }
 
-        for (String item : items) {
-            String[] parts = item.split(" ");
-            String name = parts[0];
-            int quantity = Integer.parseInt(parts[1]);
 
-            if (salesArray.containsKey(name)) {
-                int currentQuantity = salesArray.get(name);
-                salesArray.put(name, currentQuantity + quantity);
-            } else {
-                salesArray.put(name, quantity);
-            }
+    public String executeCommand(String command) {
 
+        if (command.charAt(0) == '1') {
+            String addParameter = command.substring(1).trim();
+            add(addParameter);
         }
-        List<String> result = new ArrayList<>();
-        salesArray.entrySet().stream()
-                .sorted(Comparator.comparing(Map.Entry<String, Integer>::getValue).reversed()
-                        .thenComparing(Map.Entry::getKey))
-                .forEach(entry -> result.add(entry.getKey() + " " + entry.getValue()));
 
-        return result.toArray(new String[0]);
+        if (command.charAt(0) == '2') {
+            String[] parts = command.split(" ");
+            String parameter = parts[1];
+            remove(parameter);
+        }
 
+        if (command.charAt(0) == '3') {
+            String[] parts = command.split(" ");
+            String parameter = parts[1];
+            return getCharacter(parameter);
+        }
+
+        if (command.charAt(0) == '4') {
+            undo();
+        }
+
+        if (command.charAt(0) == '5') {
+            redo();
+        }
+
+        return text.toString();
+    }
+
+    private void add(String string) {
+        if(undoStack.size() == 2){
+            undoStack.pollFirst();
+            undoStack.addLast(text.toString());
+        } else {
+            undoStack.addLast(text.toString());
+        }
+        text.append(string);
+        redoStack.clear();
+
+    }
+
+    private void remove(String count) {
+        int numToRemove = Integer.parseInt(count);
+        if (numToRemove >= text.length()) {
+            if(undoStack.size() == 2){
+                undoStack.pollFirst();
+                undoStack.addLast(text.toString());
+            } else {
+                undoStack.addLast(text.toString());
+            }
+            text.setLength(0);
+            redoStack.clear();
+        } else {
+            if(undoStack.size() == 2){
+                undoStack.pollFirst();
+                undoStack.addLast(text.toString());
+            } else {
+                undoStack.addLast(text.toString());
+            }
+            text.setLength(text.length() - numToRemove);
+            redoStack.clear();
+        }
+    }
+
+    private String getCharacter(String index) {
+        int idx = Integer.parseInt(index);
+        if (idx < text.length()) {
+            return String.valueOf(text.charAt(idx));
+        }
+        return "";
+    }
+
+    private void undo() {
+        if (undoStack.size() == 1){
+            redoStack.push(text.toString());
+            text = new StringBuilder(undoStack.peek());
+        }
+        else{
+            redoStack.push(text.toString());
+            text = new StringBuilder(undoStack.pollLast());
+        }
+    }
+
+    private void redo() {
+        if (!redoStack.isEmpty()) {
+            undoStack.addLast(text.toString());
+            text = new StringBuilder(redoStack.pop());
+        }
     }
 }
