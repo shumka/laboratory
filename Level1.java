2a2d0b22e4a3be89d74400e9fa8e408e2f89f516
Andre <a.shumilkin@outlook.com>
2023-10-20 07:48:03 +0000

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -1,38 +1,30 @@
+import java.util.*;
+
 public class Level1 {
-    
-    public static String BiggerGreater(String input) {
-        char[] magicWord = input.toCharArray();
-        int N = magicWord.length;
-        int i = N - 2;
-        while(i >= 0 && magicWord[i] >= magicWord[i+1]){
-            i--;
+    public static boolean SherlockValidString(String s) {
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+        Set<Integer> counts = new HashSet<>(charCount.values());
+
+        if (counts.size() == 1) {
+            return true;
         }
 
-        if (i == -1){
-            return "";
+        if (counts.size() == 2) {
+            int minCount = Integer.MAX_VALUE;
+            int maxCount = Integer.MIN_VALUE;
+            for (int count : counts) {
+                minCount = Math.min(minCount, count);
+                maxCount = Math.max(maxCount, count);
+            }
+            if ((minCount == 1 && Collections.frequency(charCount.values(), minCount) == 1) ||
+                    (maxCount - minCount == 1 && Collections.frequency(charCount.values(), maxCount) == 1)) {
+                return true;
+            }
         }
-        
-        int j = N - 1;
 
-        while (magicWord[j] <= magicWord[i]) {
-            j--;
-        }
-
-        char temp = magicWord[i];
-        magicWord[i] = magicWord[j];
-        magicWord[j] = temp;
-
-        
-        int left = i + 1;
-        int right = N - 1;
-
-        while (left < right) {
-            temp = magicWord[left];
-            magicWord[left] = magicWord[right];
-            magicWord[right] = temp;
-            left++;
-            right--;
-        }
-        return new String(magicWord);
+        return false;
     }
 }
