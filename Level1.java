1f20c61b4b7b5cfee709c2127dcafd34d109516f
Andre <a.shumilkin@outlook.com>
2023-10-25 11:41:10 +0000

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -1,28 +1,31 @@
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Level1 {
-    public static boolean TransformTransform(int A[], int N) {
-        List<Integer> B = doubleTransform(A, N);
+    public static boolean TransformTransform(int[] A, int N) {
+        List<Integer> B = IntStream.of(A)
+                .boxed()
+                .collect(Collectors.toList());
+        B = S(B);
+        B = S(B);
         int sum = 0;
         for (int num : B) {
             sum += num;
         }
         return sum % 2 == 0;
     }
-
-    public static List<Integer> doubleTransform(int A[], int N) {
-
+    
+    public static List<Integer> S(List<Integer> A) {
         List<Integer> B = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-
-        for (int i = 0; i < N - 1; i++) {
-            temp.clear();
-            for (int j = 0; j < N - 1 - i; j++) {
+        for (int i = 0; i < A.size(); i++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int j = 0; j < A.size() - i; j++) {
                 int k = i + j;
                 int max = 0;
                 for (int l = j; l <= k; l++) {
-                    if (A[l] > max) {
-                        max = A[l];
+                    if (A.get(l) > max) {
+                        max = A.get(l);
                     }
                 }
                 temp.add(max);
@@ -31,6 +34,5 @@ public class Level1 {
         }
         return B;
     }
-
 }
 
