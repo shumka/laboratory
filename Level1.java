1ab7cd2796b424cc6032cbc8b3ff709b6796380a
Andre <a.shumilkin@outlook.com>
2023-10-26 08:59:21 +0000

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -1,38 +1,31 @@
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import static java.lang.Character.getNumericValue;
 
 public class Level1 {
-    public static boolean TransformTransform(int[] A, int N) {
-        List<Integer> B = IntStream.of(A)
-                .boxed()
-                .collect(Collectors.toList());
-        B = S(B);
-        B = S(B);
-        int sum = 0;
-        for (int num : B) {
-            sum += num;
-        }
-        return sum % 2 == 0;
-    }
-    
-    public static List<Integer> S(List<Integer> A) {
-        List<Integer> B = new ArrayList<>();
-        for (int i = 0; i < A.size(); i++) {
-            List<Integer> temp = new ArrayList<>();
-            for (int j = 0; j < A.size() - i; j++) {
-                int k = i + j;
-                int max = 0;
-                for (int l = j; l <= k; l++) {
-                    if (A.get(l) > max) {
-                        max = A.get(l);
+    public static boolean white_walkers(String village) {
+        int walkerCount = 0;
+        boolean flag = false;
+
+        for (int i = 0; i < village.length(); i++){
+            if(Character.isDigit(village.charAt(i)) && i != village.length() ){
+                for(int j = i+1; j < village.length(); j++){
+                    if(Character.isDigit(village.charAt(j))  &&
+                            ( getNumericValue(village.charAt(i)) + getNumericValue(village.charAt(j))  == 10)){
+                        for(int x=i,y=j; x < y; x++ ){
+                            if(village.charAt(x) == '=') {
+                                walkerCount += 1;
+                            }
+                        }
+                        if(walkerCount >= 3){
+                            flag = true;
+                        } else {
+                            flag = false;
+                        }
+                        walkerCount = 0;
                     }
                 }
-                temp.add(max);
             }
-            B.addAll(temp);
         }
-        return B;
+
+        return flag;
     }
 }
-
