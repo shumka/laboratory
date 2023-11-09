814f97ebd7cc1f878c1e25f75229f903969f3e33
Andre <a.shumilkin@outlook.com>
2023-11-09 09:18:58 +0000

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -60,21 +60,5 @@ public class Recursion {
         System.out.println(list.get(index));
         sixRecursion(list, index + 2);
     }
-
-    private static int sevenRecursion(int[] arr) {
-        int count = 0;
-        for (int i = 0; i < arr.length - 1; i++)
-            if (arr[i] < arr[i + 1]) {
-                int temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                count++;
-            }
-        if (count > 0) {
-            sevenRecursion(arr);
-        }
-        return arr[1];
-    }
     
-
 }
