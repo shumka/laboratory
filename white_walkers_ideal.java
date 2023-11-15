fe88588050becc75b7b4d83a3bae6c37752a8402
Andre <a.shumilkin@outlook.com>
2023-11-15 06:50:20 +0000

    Update white_walkers_ideal.java

--- white_walkers_ideal.java
+++ white_walkers_ideal.java
@@ -1,1 +1,24 @@
-1
+public static boolean white_walkers_ideal(String village) {
+        int dig = 0;
+        int wnum = 0;
+        boolean has10 = false;
+
+        for (char ch : village.toCharArray()) {
+            if(Character.isDigit(ch)){
+                if(Character.getNumericValue(ch) + dig == 10) {
+                    if(wnum != 3){
+                        return false;
+                    } else {
+                        has10 = true;
+                    }
+                }
+                dig = Character.getNumericValue(ch);
+                wnum = 0;
+            } else if (ch == '='){
+                wnum += 1;
+            }
+        }
+
+        return has10;
+
+    }
